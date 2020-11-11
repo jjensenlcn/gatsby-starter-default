1e8eabe586c585a4d1147a5c9114df6e38033512
jjensenlcn <44657030+jjensenlcn@users.noreply.github.com>
2020-11-11 01:38:15 -0800

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>!!!Gatsby</p>
-    <p>Now go build something greats.</p>
+    <p>Now go build something greats...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
